4c5212669c4d3ab51a567d0949aad0bfc5060d6c
Yeong Sheng, Tan <yeongsheng.tan@ruckuswireless.com>
2016-12-24 21:12:19 +0800

    feat: initial identicon elixir project commit

---
+++ lib/identicon.ex
@@ -0,0 +1,33 @@
+defmodule Identicon do
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{ hex: hex } = image) do
+    grid = hex
+           |> Enum.chunk(3)
+           |> Enum.map(&mirror_row/1)
+           |> List.flatten
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{ hex: [r,g,b | _tail] } = image) do
+    %Identicon.Image{ image | color: {r,g,b} }
+  end
+
+  def hash_input(input) do
+    hex =
+        :crypto.hash(:md5, input)
+        |> :binary.bin_to_list
+
+     %Identicon.Image{ hex: hex }
+  end
+end
